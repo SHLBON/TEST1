da64bc997c9867e0e332fa904408df3de07f1b04
SHLBON <43978213+SHLBON@users.noreply.github.com>
2018-10-09 11:12:23 +0200

    AHMED

---
+++ TEST NO.1/Assets/PLAYER.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PLAYER : MonoBehaviour {
+	[Header("ده بتاع التحكم ")]
+	public Rigidbody2D rb;
+
+	[Header("ده بتاع الطلوع لفوق")]
+	public int yspeed;
+	public int xspeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+	}
+	void FixedUpdate(){
+		rb.velocity =new Vector2(rb.velocity.x,yspeed);
+		
+		
+		
+		if (Input.GetAxisRaw("Horizontal")>0){
+			rb.velocity =new Vector2(xspeed,rb.velocity.y);
+		}else if (Input.GetAxisRaw("Horizontal")<0){
+			rb.velocity =new Vector2(-xspeed,rb.velocity.y);
+		}else{
+			rb.velocity =new Vector2(0,rb.velocity.y);
+		}
+
+	}
+}
---
+++ TEST NO.1/Assets/PlayerCntrol.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCntrol : MonoBehaviour {
+	private Rigidbody2D Rb;
+
+	[Header("السرعه والتحكم")]
+	public int Xspeed;
+	public float Yspeed;
+	// Use this for initialization
+	void Start () {
+		Rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate	 (){
+		//A للاتجاه للاعلى بسرعه سابته 
+		Rb.velocity =new Vector2 (Rb.velocity.x,Yspeed);
+
+		//a الاتجاه يمين و يسار
+		if (Input.GetAxisRaw("Horizontal")<0){
+			Rb.velocity =new Vector2 (-Xspeed,Rb.velocity.y);
+		}
+		else if (Input.GetAxisRaw("Horizontal")>0){
+			Rb.velocity = new Vector2 (Xspeed,Rb.velocity.y);
+		}else{
+			Rb.velocity = new Vector2 (0,Rb.velocity.y);
+		}
+		//------------------
+
+	}
+}
